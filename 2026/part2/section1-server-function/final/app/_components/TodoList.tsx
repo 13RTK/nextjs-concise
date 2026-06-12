2625deb7a0959adba9a90264f84a8e9f6fc75392
alex <907785012@qq.com>
2026-06-12 21:56:41 +0800

    add starter of section2-1

---
+++ 2026/part2/section1-server-function/final/app/_components/TodoList.tsx
@@ -0,0 +1,20 @@
+async function TodoList() {
+  async function getTodos() {
+    const res = await fetch('http://localhost:3000/api/todos');
+    const todos = await res.json();
+
+    return todos;
+  }
+
+  const todos = await getTodos();
+
+  return (
+    <ul>
+      {todos.map((todo: any) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
